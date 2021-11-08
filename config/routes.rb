3771893c4036b85854ec1848ff865bcea1a8cf8a
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2021-11-08 22:04:50 +0100

    Add 'new_author_path'

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get 'new_author_path', to: 'authors#new'
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   
