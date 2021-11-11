7e0b8ea5eaea81d05e2acf7391bef20a8cbc4243
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2021-11-11 16:20:05 +0100

    Revert "Fix test"
    This reverts commit b13e91caaa04f5bf6a853c04869f94ece445ee9f.

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe "New author page", type: :feature do
   it "should exist at 'new_author_path' and render without error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-    visit new_author_path
+    visit 'new_author_path'
   end
 end
