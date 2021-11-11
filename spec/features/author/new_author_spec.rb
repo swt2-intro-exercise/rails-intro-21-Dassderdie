772987900d16bb83160832e4447124f3d91b0ddf
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2021-11-11 16:42:14 +0100

    New author page should have text input for first name, last name, and homepage #3

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,12 @@ describe "New author page", type: :feature do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
+
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    visit new_author_path
+    # these are the standard names given to inputs by the Rails form builder
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
