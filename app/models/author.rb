8135279e20f3d9714f2a1d759b91343aebd3be2c
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2021-11-13 15:21:07 +0100

    Add author tests

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ApplicationRecord
+    def initialize(first_name, last_name, website)
+        @first_name = first_name
+        @last_name = last_name
+        @website = website
+    end
+
+    def name()
+        @first_name + " " + @last_name
+    end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "should create a new author object" do
+      author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+      expect(author.first_name).to eq('Alan')
+      expect(author.last_name).to eq('Turing')
+      expect(author.website).to eq('http://wikipedia.org/Alan_Turing')
+    end
+
+    it "should return thecorrect name" do
+      author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+      expect(author.name).to eq('Alan Turing')
+    end
 end
